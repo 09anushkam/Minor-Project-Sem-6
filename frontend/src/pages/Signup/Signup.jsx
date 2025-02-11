68c659ad30eb7bdcb09e134f3fa296460fd360e7
Vivek Masuna <vivekmasuna999@gmail.com>
2025-02-11 12:50:30 +0530

    signp-page

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,25 +1,36 @@
+import * as React from 'react';
 import styles from "./styles.module.css";
 import { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import Navbar from "../../components/Navbar";
+import Footer from '../../components/Footer';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function Signup() {
 	const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [showPassword, setShowPassword] = React.useState(false);
+	const navigate = useNavigate();
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8080/auth/signup', { name, email, password });
-            alert(response.data.message);
-            navigate('/login');
-        } catch (err) {
-            alert(err.response?.data?.message || 'Registration failed');
-        }
-    };
+	const handleRegister = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post('http://localhost:8080/auth/signup', { name, email, password });
+			alert(response.data.message);
+			navigate('/login');
+		} catch (err) {
+			alert(err.response?.data?.message || 'Registration failed');
+		}
+	};
 
 	const googleAuth = () => {
 		window.open(
@@ -27,55 +38,87 @@ function Signup() {
 			"_self"
 		);
 	};
+
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleMouseUpPassword = (event) => {
+        event.preventDefault();
+    };
+
 	return (
 		<>
 			<Navbar />
 			<div className={styles.container}>
-				{/* <h1 className={styles.heading}>Signup</h1> */}
 				<div className={styles.form_container}>
 					<div className={styles.left}>
 						<img className={styles.img} src="/signup.jpg" alt="signup" />
 					</div>
 					<div className={styles.right}>
-						<h2 className={styles.from_heading}>Create your account</h2>
+						<h2 className={styles.from_heading}>Create your Account</h2>
 						<form onSubmit={handleRegister}>
-							<input
-								type="text"
-								placeholder="Name"
+							<TextField
+								size='small'
 								className={styles.input}
+								type="text"
+								id="outlined-basic"
+								label="Name"
+								variant="outlined"
 								value={name}
 								onChange={(e) => setName(e.target.value)}
 								required
 							/>
-							<input
-								type="email"
-								placeholder="Email"
+							<TextField
+								size='small'
 								className={styles.input}
+								type="email"
+								id="outlined-basic"
+								label="Email"
+								variant="outlined"
 								value={email}
 								onChange={(e) => setEmail(e.target.value)}
 								required
 							/>
-							<input
-								type="password"
-								placeholder="Password"
-								className={styles.input}
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								required
-							/>
-							<button type="submit" className={styles.btn}>Signup</button>
+							<FormControl size='small' variant="outlined" type="password" className={styles.input} value={password} onChange={(e) => setPassword(e.target.value)} required>
+								<InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+								<OutlinedInput
+									id="outlined-adornment-password"
+									type={showPassword ? 'text' : 'password'}
+									endAdornment={
+										<InputAdornment position="end">
+											<IconButton
+												aria-label={
+													showPassword ? 'hide the password' : 'display the password'
+												}
+												onClick={handleClickShowPassword}
+												onMouseDown={handleMouseDownPassword}
+												onMouseUp={handleMouseUpPassword}
+												edge="end"
+											>
+												{showPassword ? <VisibilityOff /> : <Visibility />}
+											</IconButton>
+										</InputAdornment>
+									}
+									label="Password"
+								/>
+							</FormControl>
+							<button type="submit" className={styles.btn}>Sign Up</button>
 						</form>
-						<p className={styles.text}>Or Continue With</p>
+						<p className={styles.text}>Or</p>
 						<button className={styles.google_btn} onClick={googleAuth}>
-							<img src="/google.png" alt="google icon" />
-							<span>Google</span>
+							<img src="/google.webp" alt="google icon" />
+							<span>Sign In With Google</span>
 						</button>
-						<p className={styles.text}>
+						<p className={styles.textend}>
 							Already have an account ? <Link to="/login">Log In</Link>
 						</p>
 					</div>
 				</div>
 			</div>
+			<Footer />
 		</>
 	);
 }
