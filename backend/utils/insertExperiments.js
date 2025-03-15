532ac3876e97c2cf7f8b13d6a94dd48822f53e96
Vivek Masuna <vivekmasuna999@gmail.com>
2025-03-15 11:30:10 +0530

    ...

--- backend/utils/insertExperiments.js
+++ backend/utils/insertExperiments.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config({ path: '../.env' });
 
 // Connect to MongoDB
-const MONGO_URL = "mongodb://127.0.0.1:27017/SMA";
+const MONGO_URL = process.env.ATLAS_DB;
 
 main()
     .then((res) => {
