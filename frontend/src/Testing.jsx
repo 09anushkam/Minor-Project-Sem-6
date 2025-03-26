23773d9c9664065990767b461d1bdfb6d1b91256
Vivek Masuna <vivekmasuna999@gmail.com>
2025-03-26 14:14:12 +0530

    exp2 implementation frontend

--- frontend/src/Testing.jsx
+++ frontend/src/Testing.jsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
-import ScrapeForm1 from "./components/scrapeForm1";
-import ScrapeForm from "./components/ScrapeForm";
-import ScrapeResults from "./components/ScrapeResults";
+// import { useState } from "react";
 
 function Testing() {
-  const [results, setResults] = useState(null);
-  console.log("Scraped results are: ", results);
 
   return (
     <div>
-      <h1>Social Media Data Scraper</h1>
-      {/* <ScrapeForm setResults={setResults} /> */}
-      {/* {results && <ScrapeResults results={results} />} */}
-
-      <ScrapeForm1 />
+      
     </div>
   );
 }
