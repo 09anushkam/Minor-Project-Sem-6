430bf834b9ebf0304e63177062c0b5f2c9bb1f06
Vivek Masuna <vivekmasuna999@gmail.com>
2025-05-08 23:41:30 +0530

    Remove backend files from frontend branch

--- backend/models/ScrapedData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ScrapedDataSchema = new mongoose.Schema({
-    method: String,
-    data: Array,
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("ScrapedData", ScrapedDataSchema);
